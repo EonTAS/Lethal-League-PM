807d6122496c6a23933d4b56b24816ba58e1d473
Eon <oglesby.mo@gmail.com>
2021-01-29 14:25:04 +0000

    Removed Slight extra speed ball adds when hit Made all wall collisions elastic made rudimentary stage box

---
+++ Volleyball.asm
@@ -0,0 +1,8 @@
+Elastic Collisions and no Caps 
+float 1.0 @ $9102B77C #= wallbounce multiplier horziontal, set to 1
+float 1.0 @ $9102B780 #= wallbounce multiplier vertical, set to 1
+float 1.0 @ $9102B784 #= cieling bounce mult horiz
+float 1.0 @ $9102B788 #= cieling bounce mult horiz
+
+float 0.0 @ $9102b72c #= vertical max cap
+float 0.0 @ $9102b79c #= horizontal max cap
